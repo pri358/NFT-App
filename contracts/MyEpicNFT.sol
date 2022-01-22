70f6b7e3906909e73a1f01d71e5e665588e5c3ec
Priyanshi Jain <priyanshi.jain030@gmail.com>
2022-01-22 19:53:55 +0530

    generating random nft

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/utils/Strings.sol";
 // console logging lib provided by hardhat
 import "hardhat/console.sol";
 // We first import some OpenZeppelin Contracts. ERC721: NFT Standard, OppenZeplin provides boiler plate code
@@ -16,18 +17,60 @@ contract MyEpicNFT is ERC721URIStorage {
     // unique identifier for NFTs
     Counters.Counter private _tokenIds; 
 
+    // So, we make a baseSvg variable here that all our NFTs can use.
+    string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+    // harry potter 
+    string[] firstWords = ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"];
+    string[] secondWords = ["Harry", "Hermoine", "Ron", "Draco", "Hagrid", "Dumbledore", "Snape", "Dobby", "Voldemort", "Black", "McGonagall", "Ginny", "George"];
+    string[] thirdWords = ["DeathlyHallows", "Philosopher", "Chamber", "Secrets", "Stone", "Goblet", "Fire", "Prisoner", "Azkaban", "Phoenix", "HalfBlood" ,"Prince"];
+
     constructor() ERC721 ("SquareNFT", "SQUARE") {
         console.log("This is my NFT contract. Whoa!");
+    }
+
+    function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
+        // I seed the random generator. More on this in the lesson. 
+        uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
+        // Squash the # between 0 and the length of the array to avoid going out of bounds.
+        rand = rand % firstWords.length;
+        return firstWords[rand];
+    }
+
+    function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
+        uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
+        rand = rand % secondWords.length;
+        return secondWords[rand];
+    }
+
+    function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
+        uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
+        rand = rand % thirdWords.length;
+        return thirdWords[rand];
+    }
+
+    function random(string memory input) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(input)));
     }
 
     function makeAnEpicNFT() public {
         uint256 newItemId = _tokenIds.current();
 
-    // giving the current NFT token id to the address callibng the function
+        string memory first = pickRandomFirstWord(newItemId);
+        string memory second = pickRandomSecondWord(newItemId);
+        string memory third = pickRandomThirdWord(newItemId);
+
+        // I concatenate it all together, and then close the <text> and <svg> tags.
+        string memory finalSvg = string(abi.encodePacked(baseSvg, first, second, third, "</text></svg>"));
+        console.log("\n--------------------");
+        console.log(finalSvg);
+        console.log("--------------------\n");
+
+        // giving the current NFT token id to the address callibng the function
         _safeMint(msg.sender, newItemId);
 
         // tokenURI: where NFT data lives
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/U569");
+        _setTokenURI(newItemId, "blah");
         console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
 
         _tokenIds.increment();
